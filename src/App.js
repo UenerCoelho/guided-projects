3f7f5468e4f819b512f724b3d6bf51dc472d30ef
Uener Coelho <uenercoelho2@hotmail.com>
2023-04-01 23:30:54 -0400

    Jornada Dev aula 05

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
+import React, { useEffect, useState } from "react"
 import "./App.css"
 import Video from "./pages/Video"
+import db from "./config/firebase"
+import { collection, getDocs } from "firebase/firestore/lite"
 
 function App() {
+  const [video, setVideos] = useState([])
+
+  async function getVideos() {
+    const videoCollection = collection(db, "videos")
+    const videosSnapshot = await getDocs(videoCollection)
+    const videosList = videosSnapshot.docs.map(doc => doc.data())
+    setVideos(videosList)
+  }
+
+  useEffect(() => {
+    getVideos()
+  }, [])
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-          likes={0}
-          messages={0}
-          shares={0}
-          name="Uener Coelho"
-          description="Descrição aqui"
-          music="Music Name"
-          url="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
-        />
-        <Video
-          likes={0}
-          messages={0}
-          shares={0}
-          name="Uener Coelho"
-          description="Descrição aqui"
-          music="Music Name"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
-        />
-        <Video
-          likes={0}
-          messages={0}
-          shares={0}
-          name="Uener Coelho"
-          description="Descrição aqui"
-          music="Music Name"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
-        />
+        {video.map(item => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          )
+        })}
+        {video.map(item => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          )
+        })}
+        {video.map(item => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          )
+        })}
       </div>
     </div>
   )

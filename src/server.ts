7a6ef5955f68ec08f42b6abf6db5dcfd5ecb4562
Uener Coelho <uenercoelho2@hotmail.com>
2023-05-15 20:21:51 -0400

    first commit ^^

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World!'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('🤞HTTP server running on http://localhost:333')
+})

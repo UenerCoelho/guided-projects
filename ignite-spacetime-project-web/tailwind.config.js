244f6b74c40abd2c9f91b21cc7920cb81efbfb4e
Uener Coelho <uenercoelho2@hotmail.com>
2023-05-18 20:57:01 -0400

    FrontEnd part 1

--- ignite-spacetime-project-web/tailwind.config.js
+++ ignite-spacetime-project-web/tailwind.config.js
@@ -81,6 +81,10 @@ module.exports = {
       backgroundSize: {
         stripes: '100% 8px',
       },
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
       blur: {
         full: '194px',
       },

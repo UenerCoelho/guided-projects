190889eba853b1bc3552d870e608bdf0006f4379
Uener Coelho <uenercoelho2@hotmail.com>
2023-05-23 19:54:02 -0400

    FrontEnd part 2

--- ignite-spacetime-project-web/src/app/page.tsx
+++ ignite-spacetime-project-web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import { Copyright } from '@/components/Copyright'
+import { Hero } from '@/components/Hero'
+import { Right } from '@/components/Right'
+import { Signin } from '@/components/Signin'
 
 export default function Home() {
   return (
@@ -13,66 +14,13 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes" />
 
-        {/* Sign In */}
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 transition-colors hover:bg-gray-200">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="leading0snug max-w-[140px] text-sm">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-        {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            href=""
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-800"
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blanck"
-            rel="noreferrer"
-            href="https://rockeseat.com.br"
-            className="underline hover:text-gray-100"
-          >
-            Rocketseat
-          </a>
-        </div>
+        <Signin />
+        <Hero />
+        <Copyright />
       </div>
 
       {/* Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="#" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
-      </div>
+      <Right />
     </main>
   )
 }

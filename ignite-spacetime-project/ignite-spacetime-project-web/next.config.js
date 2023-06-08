f16010297fa6e45d92f19ff93ea1506bee843a14
Uener Coelho <uenercoelho2@hotmail.com>
2023-06-07 20:31:32 -0400

    ignite-space-time added

---
+++ ignite-spacetime-project/ignite-spacetime-project-web/next.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'avatars.githubusercontent.com', '192.168.1.36',
+    ],
+  }
+}
+
+module.exports = nextConfig

58e1f561f3045d852964cb0084e14aefa493d125
Uener Coelho <uenercoelho2@hotmail.com>
2023-06-08 15:24:05 -0400

    boracodar added

--- ignite-spacetime-project/ignite-spacetime-project-server/README.md
+++
@@ -1,4 +0,0 @@
-# ignite-spacetime-project-server
-
-
-<!-- Parei no Minuto 1:37:24 Aula 02 -->
--- ignite-spacetime-project/ignite-spacetime-project-server/src/lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PrismaClient } from '@prisma/client'
-
-export const prisma = new PrismaClient({
-  log: ['query'],
-})

0cb58b44a06942adc1c5e7a9a04d4a9824d61a2d
Uener Coelho <uenercoelho2@hotmail.com>
2023-06-20 20:58:29 -0400

    portfolio patrick moz finished

--- portfolios/portfolio-01-patrick-moz/assets/js/main.js
+++ portfolios/portfolio-01-patrick-moz/assets/js/main.js
@@ -64,9 +64,9 @@ const sendEmail = e => {
         }, 5000);
 
         // Clear input fields
-        contactForm.reset()
+        contactForm.reset();
       },
-      
+
       () => {
         // Show error message
         contactForm.textContent = 'Message not sent (service error) ❌';
@@ -77,7 +77,51 @@ const sendEmail = e => {
 contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup
+  this.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+    const sectionsClass = document.querySelector(
+      '.nav__menu a[href*=' + sectionId + ']'
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 300,
+  reset: true // Animation repeat
+});
+
+sr.reveal(
+  `.home__data, .home__social, .contact__container, .footer__container`
+);
+sr.reveal(`.home__image`, { origin: 'bottom' });
+sr.reveal(`.about__data, .skills__data`, { origin: 'left' });
+sr.reveal(`.about__image, .skills__content`, { origin: 'right' });
+sr.reveal(`.services__card, .projects__card`, { interval: 100 });

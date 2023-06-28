06cdb6da4cf516e1d8bd1ad7b3e98e47358ffe0b
Uener Coelho <uenercoelho2@hotmail.com>
2023-06-27 20:12:55 -0400

    portfolio chris by bedimcode

--- portfolios/portfolio-002-chris-bedimcode/assets/js/main.js
+++ portfolios/portfolio-002-chris-bedimcode/assets/js/main.js
@@ -1,35 +1,49 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
 
 /*** ===-===-=== Menu Show ===-===-=== ***/
 // Validate if constant exists
 if (navToggle) {
   navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+    navMenu.classList.add('show-menu')
+  })
 }
 
 /*** ===-===-=== Menu Hidden ===-===-=== ***/
 // Validate if constant exists
 if (navClose) {
   navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+    navMenu.classList.remove('show-menu')
+  })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () => {
-  const navMenu = document.getElementById('nav-menu');
+  const navMenu = document.getElementById('nav-menu')
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
-};
-navLink.forEach(n => n.addEventListener('click', linkAction));
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
+var swiperProjects = new Swiper('.projects__container', {
+  loop: true,
+  spaceBetween: 24,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
